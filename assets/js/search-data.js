842024395eac9418de98a712409889bb0008f278
zhexuphd <139696208+zhexuphd@users.noreply.github.com>
2025-12-17 19:18:08 +0000

    Deploying to gh-pages from @ zhexuphd/zhexuphd.github.io@45b4c732aad3ea2bda7b7a18b1cfb7ce7378b970 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-service",
           title: "service",
-          description: "Doing my best to keep up with academic service duties.",
+          description: "doing my best to keep up with academic service duties.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/service/";
@@ -52,6 +52,9 @@ ninja.data = [{
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-excited-to-join-the-department-of-media-and-communication-at-lmu-munich-as-a-research-associate",
+          title: 'Excited to join the Department of Media and Communication at LMU Munich as...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
