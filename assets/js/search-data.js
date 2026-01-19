9a9d75117c4f95dde6af04ddccea6047064e8406
zhexuphd <139696208+zhexuphd@users.noreply.github.com>
2026-01-19 20:50:37 +0000

    Deploying to gh-pages from @ zhexuphd/zhexuphd.github.io@c3dbaf3024e021152f030828cbe96416de528a48 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -97,11 +97,6 @@ ninja.data = [{
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
             },},{
       id: 'light-theme',
       title: 'Change theme to light',
